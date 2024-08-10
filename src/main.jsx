437f18469b4ae149b2135694f0c8aa513f3a1e7c
fatimah-mse <fatimahmse83@gmail.com>
2024-08-10 12:17:01 +0300

    edit-basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/web">
+    <BrowserRouter basename="/Web-Working">
       <App />
     </BrowserRouter>
   </React.StrictMode>
